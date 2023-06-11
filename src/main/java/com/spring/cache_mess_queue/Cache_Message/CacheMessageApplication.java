4b5c12efe517fc7d5cdb20947ad7e8b393134b9c
imshashikumarGH <imshashikumar01@gmail.com>
2023-06-11 21:01:14 +0530

    Basic Spring-boot application

---
+++ src/main/java/com/spring/cache_mess_queue/Cache_Message/CacheMessageApplication.java
@@ -0,0 +1,13 @@
+package com.spring.cache_mess_queue.Cache_Message;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CacheMessageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CacheMessageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/cache_mess_queue/Cache_Message/CacheMessageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.cache_mess_queue.Cache_Message;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CacheMessageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
