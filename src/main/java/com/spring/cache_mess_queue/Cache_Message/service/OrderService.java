13afb4373a3ea864d78e2b5c1a8fe2aa252e4456
imshashikumarGH <imshashikumar01@gmail.com>
2023-06-12 13:14:40 +0530

    Feature AMQP Config: to push message via API call

---
+++ src/main/java/com/spring/cache_mess_queue/Cache_Message/service/OrderService.java
@@ -0,0 +1,10 @@
+package com.spring.cache_mess_queue.Cache_Message.service;
+
+import com.spring.cache_mess_queue.Cache_Message.model.Order;
+
+
+public interface OrderService {
+
+    String orderProcessedService(Order order);
+
+}
---
+++ src/main/java/com/spring/cache_mess_queue/Cache_Message/service/serviceImpl/OrderServiceImpl.java
@@ -0,0 +1,30 @@
+package com.spring.cache_mess_queue.Cache_Message.service.serviceImpl;
+
+import com.spring.cache_mess_queue.Cache_Message.config.AmqpConfiguration;
+import com.spring.cache_mess_queue.Cache_Message.model.Order;
+import com.spring.cache_mess_queue.Cache_Message.model.OrderProcessed;
+import com.spring.cache_mess_queue.Cache_Message.service.OrderService;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+public class OrderServiceImpl implements OrderService {
+    @Autowired
+    private RabbitTemplate template;
+
+    @Override
+    public String orderProcessedService(Order order) {
+        order.setUuid(UUID.randomUUID().toString());
+        template.convertAndSend(AmqpConfiguration.EXCHANGE,
+                AmqpConfiguration.ROUTING_KEY,
+                OrderProcessed.builder()
+                        .order(order)
+                        .status("PROCESSED")
+                        .message("Success")
+                        .build());
+        return "Success !!";
+    }
+}
